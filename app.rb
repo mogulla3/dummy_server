8ca145340045d771ab55a45f5690f0d9d8ad1561
mogulla3 <hs.iarn3@gmail.com>
2018-03-17 09:27:26 +0900

    update app.rb

--- app.rb
+++ app.rb
@@ -1,16 +1,34 @@
-require 'sinatra'
+require "sinatra"
 
 class MyApp < Sinatra::Base
-  get '/' do
-    'Hello, Sinatra'
+  get "/" do
+    [200, req_info("GET").to_json]
   end
 
-  get '/error' do
-    [500, 'Error occurred']
+  post "/" do
+    [200, req_info("POST").to_json]
   end
 
-  get '/slow' do
-    sleep 10
-    'Slow response'
+  get "/400" do
+    [400, req_info("clienet error").to_json]
+  end
+
+  get "/500" do
+    [500, req_info("server error").to_json]
+  end
+
+  get "/slow" do
+    sleep 5
+    [200, req_info("slow response").to_json]
+  end
+
+  def req_info(message = "")
+    {
+      message: message,
+      method: request.request_method,
+      path: request.path_info,
+      query_string: request.query_string,
+      body: request.body
+    }
   end
 end
